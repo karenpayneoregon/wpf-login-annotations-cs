7aca121954682f89779f185c1f28e7d7d7fb6990
Karen Payne <Karen.1.Payne@oregon.gov>
2021-06-22 09:34:52 -0700

    refactors and elimiated several unused methods

--- ValidateLoginCore/Classes/CommonDialogs.cs
+++ ValidateLoginCore/Classes/CommonDialogs.cs
@@ -12,27 +12,7 @@ namespace ValidateLoginCore.Classes
         /// <param name="pMessage"></param>
         /// <param name="pTitle">Title which defaults to 'Question'</param>
         /// <returns></returns>
-        public static bool Question(string pMessage, string pTitle = "Question")
-        {
-            return (Show(pMessage, pTitle, MessageBoxButton.YesNo, MessageBoxImage.Question, MessageBoxResult.No) == MessageBoxResult.Yes);
-        }
-        /// <summary>
-        /// Ask a question with the ability to define the default button to Yes or No
-        /// </summary>
-        /// <param name="text"></param>
-        /// <param name="title">Title for message box</param>
-        /// <param name="defaultButton"></param>
-        /// <returns></returns>
-        public static bool Question(string text, string title, MessageBoxResult defaultButton)
-        {
-            MessageBoxResult button = 0;
-            if (defaultButton == MessageBoxResult.No)
-            {
-                button = MessageBoxResult.No;
-            }
-
-            return (Show(text, title, MessageBoxButton.YesNo, MessageBoxImage.Question, button) == MessageBoxResult.Yes);
-        }
+        public static bool Question(string pMessage, string pTitle = "Question") => (Show(pMessage, pTitle, MessageBoxButton.YesNo, MessageBoxImage.Question, MessageBoxResult.No) == MessageBoxResult.Yes);
         /// <summary>
         /// Ask a question with OK and Cancel where the default button is Cancel
         /// </summary>
@@ -40,9 +20,6 @@ namespace ValidateLoginCore.Classes
         /// <param name="title">Title for message box</param>
         /// <param name="defaultButton"></param>
         /// <returns></returns>
-        public static bool QuestionWithCancel(string text, string title, MessageBoxResult defaultButton = MessageBoxResult.Cancel)
-        {
-            return Show(text, title, MessageBoxButton.OKCancel, MessageBoxImage.Question, defaultButton) == MessageBoxResult.OK;
-        }
+        public static bool QuestionWithCancel(string text, string title, MessageBoxResult defaultButton = MessageBoxResult.Cancel) => Show(text, title, MessageBoxButton.OKCancel, MessageBoxImage.Question, defaultButton) == MessageBoxResult.OK;
     }
 }
